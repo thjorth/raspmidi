8b2723a3a32c63b614971d3d218e054db99dc4c0
Thomas Hjorth <thomas.hjorth@valtech.com>
2017-04-01 14:40:45 +0200

    *

--- midi.py
+++ midi.py
@@ -82,6 +82,7 @@ class MidiInputHandler(object):
             m5out.send_message([192, m5_pc])
             
         except:
+            print "Unexpected error:", sys.exc_info()[0]
             zoomout.send_message(message)
             m5out.send_message(message)
 
